1d0a7c62ea7bb1746c458e701804c3c35e7b07c4
SwissNuff <39580184+SwissNuff@users.noreply.github.com>
2021-12-09 17:20:27 +0800

    (Feat) RoundButton

---
+++ src/components/atoms/PrimaryButton/index.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { COLOR } from "../../constants/color";
+
+const RoundButton = styled.button`
+  background-color: transparent;
+  border: 3px solid ${COLOR.mainBlue[500]};
+  height: 3rem;
+  min-width: 12em;
+  padding: 0 3rem;
+  color: ${COLOR.mainBlue[500]};
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 1em;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 65px;
+  margin: auto auto;
+
+  &:hover {
+    background-color: ${COLOR.mainBlue[500]};
+    color: white;
+    transition-duration: 0.3s;
+  }
+`;
+
+export default RoundButton;
