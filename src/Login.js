17101cb41e5eff0d9a682ff0558fe40402b70a4e
shannaurelle <shannaurelleg@gmail.com>
2021-12-09 15:29:09 +0800

    fixed onClick

--- src/Login.js
+++ src/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Email" onclick=
+                <Form.Control type="email" placeholder="Email" onClick=
                 {   (event) => { setEmail(event.target.value); }   }/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
@@ -41,7 +41,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" onclick=
+                <Form.Control type="password" placeholder="Password" onClick=
                 {   (event) => { setPassword(event.target.value); }   }/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
