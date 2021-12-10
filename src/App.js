94f08d0440002911a212150c0431ca7842a03326
edmonvap <g.edmon25@gmail.com>
2021-12-10 16:39:26 +0700

    Added verification page.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {Container} from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Login from "./Login";
 import Register from "./Register";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Scratch from "./Scratch";
 
 function App() {
-
   return (
-    <Container style={{maxWidth: "480px", marginLeft: "auto", display: "flex", flexDirection: "vertical"}}>
+    <Container
+      style={{
+        maxWidth: "480px",
+        marginLeft: "auto",
+        display: "flex",
+        flexDirection: "vertical",
+      }}
+    >
       <Router>
-      <header variant="h1" className="App-header">
-      </header>
-      <Switch>
-        <Route exact path="/"> <Login />  </Route>
-        <Route exact path="/register"> <Register />  </Route>
-        <Route exact path="/scratch"> <Scratch />  </Route>
-      </Switch>
-  
+        <header variant="h1" className="App-header"></header>
+        <Switch>
+          <Route exact path="/">
+            {" "}
+            <Login />{" "}
+          </Route>
+          <Route exact path="/register">
+            {" "}
+            <Register />{" "}
+          </Route>
+          <Route exact path="/scratch">
+            {" "}
+            <Scratch />{" "}
+          </Route>
+        </Switch>
       </Router>
     </Container>
-    
   );
 }
 export default App;
---
+++ src/Verification.js
@@ -0,0 +1,7 @@
+import { Container, Button, Form, Col, Row } from "react-bootstrap";
+
+const Verification = () => {
+  return <div></div>;
+};
+
+export default Verification;
