5f0b7d92b98ab60a66a213014e2f2532401a34ed
shannaurelle <shannaurelleg@gmail.com>
2021-12-10 09:38:46 +0800

    partial custom fetch hook progress

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,29 @@
+import {useState, useEffect} from "react";
+
 const useFetch = () => {
+const [isPending, setIsPending] = useState(null);
+const {error, setError} = useState(null);
+
+useEffect( () => {
+    setTimeout(() => {
+        fetch("http://localhost:3000/")
+        .then( res => {
+            if(!res.ok){
+                throw Error("Could not fetch that resource!");
+            }
+            return res.json();
+        })
+        .then( data => {
+            setError(null);
+        })
+        .catch( err => {
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, 1000);
+
+}, []);
     
 };
+
+export default useFetch;
