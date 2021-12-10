8b668fdc6bde19a0c0954630b4c436d19a023bdf
shannaurelle <shannaurelleg@gmail.com>
2021-12-10 09:58:22 +0800

    updated custom hook (awaiting integration)

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,28 +1,37 @@
 import {useState, useEffect} from "react";
 
-const useFetch = () => {
+const useFetch = (url) => {
+const [data, setData] = useState(null);
 const [isPending, setIsPending] = useState(null);
 const {error, setError} = useState(null);
 
-useEffect( () => {
-    setTimeout(() => {
-        fetch("http://localhost:3000/")
-        .then( res => {
-            if(!res.ok){
-                throw Error("Could not fetch that resource!");
-            }
-            return res.json();
-        })
-        .then( data => {
-            setError(null);
-        })
-        .catch( err => {
-            setIsPending(false);
-            setError(err.message);
-        })
-    }, 1000);
+    useEffect( () => {
+        const AbortCont = new AbortController();
 
-}, []);
+        setTimeout(() => {
+            fetch(url, { signal: AbortCont.signal })
+            .then( res => {
+                if(!res.ok){
+                    throw Error("Could not fetch that resource!");
+                }
+                return res.json();
+            })
+            .then( data => {
+                setData(data);
+                setError(null);
+            })
+            .catch( err => {
+                if(err.name === "AbortError"){
+                    console.log("fetch aborted");
+                } else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            })
+        }, 1000);
+    }, [url]);
+
+    return {data, isPending, error};
     
 };
 
