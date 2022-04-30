996c7267fdb381ae9c2228f3393fe7a808bfa63d
vapx <g.edmon25@gmail.com>
2022-04-30 16:10:26 +0800

    test

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -13,20 +13,21 @@ const Dashboard = () => {
           <BsSearch className="text-muted" />
           <input className="search" type="text" placeholder="search" />
         </span>
-        <BsFillGearFill />
+        <BsFillGearFilasdasdasdasdl />
       </div>
+      asdasdad
       <hr></hr>
       <div className="d-flex justify-content-between">
-      <button style={{ borderRadius: "25px" }} className="btn btn-options">
-          <strong>Education</strong>
+        <button style={{ borderRadius: '25px' }} className="btn btn-options">
+          <strongasd>Education</strongasd>
         </button>
-        <button style={{ borderRadius: "25px" }} className="btn btn-options">
+        <button style={{ borderRadius: '25px' }} className="btn btn-options">
           <strong>Social</strong>
         </button>
-        <button style={{ borderRadius: "25px" }} className="btn btn-options">
+        <button style={{ borderRadius: '25px' }} className="btn btn-options">
           <strong>Employment</strong>
         </button>
-        <button style={{ borderRadius: "25px" }} className="btn btn-options">
+        <button style={{ borderRadius: '25px' }} className="btn btn-options">
           <strong>Health</strong>
         </button>
       </div>
@@ -37,48 +38,63 @@ const Dashboard = () => {
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
         </DropdownButton>
-        <a style={{ fontSize: "14px", color: "grey" }}>Show all</a>
-        <a style={{ fontSize: "14px", color: "grey" }}>Proposal</a>
-        <a style={{ fontSize: "14px", color: "grey" }}>Announcements</a>
-        <a style={{ fontSize: "14px" }}>Concerns</a>
+        <a style={{ fontSize: '14px', color: 'grey' }}>Show all</a>
+        <a style={{ fontSize: '14px', color: 'grey' }}>Proposal</a>
+        <a style={{ fontSize: '14px', color: 'grey' }}>Announcements</a>
+        <a style={{ fontSize: '14px' }}>Concerns</a>
       </div>
-      <div className="pt-5" style={{display:"flex", flexDirection:"column", gap:"1em"}}>
+      <div
+        className="pt-5"
+        style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}
+      >
         <CardPosts
-            agreeCount={580}
-            disagreeCount={420}
-            title={"Proposal for construction of more public library"}
-            name={"Juan Dela Cruz"}
-            date={"December 12, 2021"}
-            description={"Now, just imagine if we have at least one public library in each congressional district..."}
-            starCount={34}
-            topic={"proposal"}
-            imageSrc={"https://lh3.googleusercontent.com/proxy/NMKVF0unzGF-SIG3jVoRSe9j0qosZYeGZVeDSeLX7SPswVzsQiSmLRTnAZAZFJfhGg4LRbeQ4pDdB9FIT03ZwAXtijPf7Ql382k0tM3noTVWdbIjZOzzlmip2YnUCyopGDcF"}
+          agreeCount={580}
+          disagreeCount={420}
+          title={'Proposal for construction of more public library'}
+          name={'Juan Dela Cruz'}
+          date={'December 12, 2021'}
+          description={
+            'Now, just imagine if we have at least one public library in each congressional district...'
+          }
+          starCount={34}
+          topic={'proposal'}
+          imageSrc={
+            'https://lh3.googleusercontent.com/proxy/NMKVF0unzGF-SIG3jVoRSe9j0qosZYeGZVeDSeLX7SPswVzsQiSmLRTnAZAZFJfhGg4LRbeQ4pDdB9FIT03ZwAXtijPf7Ql382k0tM3noTVWdbIjZOzzlmip2YnUCyopGDcF'
+          }
         />
         <CardPosts
-            agreeCount={580}
-            disagreeCount={420}
-            title={"Christmas Clean Up Drive"}
-            name={"LGU of Pasay City"}
-            date={"December 10, 2021"}
-            description={"It has long been a challenge trying to rehabilitate the Pasig River. Environmentally it is pretty devastated..."}
-            starCount={21}
-            topic={"news"}
-            imageSrc={"https://www.adb.org/sites/default/files/styles/content_media/public/content-media/6402-philippines-cleaning-pasig-river-feature-01.jpg?itok=SOhmA1xZ"}
+          agreeCount={580}
+          disagreeCount={420}
+          title={'Christmas Clean Up Drive'}
+          name={'LGU of Pasay City'}
+          date={'December 10, 2021'}
+          description={
+            'It has long been a challenge trying to rehabilitate the Pasig River. Environmentally it is pretty devastated...'
+          }
+          starCount={21}
+          topic={'news'}
+          imageSrc={
+            'https://www.adb.org/sites/default/files/styles/content_media/public/content-media/6402-philippines-cleaning-pasig-river-feature-01.jpg?itok=SOhmA1xZ'
+          }
         />
         <CardPosts
-            agreeCount={580}
-            disagreeCount={420}
-            title={"No Manhole Cover | May nahulog na!"}
-            name={"Ronald James Dizon"}
-            date={"December 07, 2021"}
-            description={"There's an open manhole along the street of Mapayapa. Looking forward to cover this Mayor!"}
-            starCount={12}
-            topic={"concern"}
-            imageSrc={"http://4.bp.blogspot.com/-hsqL4cku7NM/VD4fvqd0zeI/AAAAAAAAXOg/Q-uukYGHEII/s1600/DSCN1266.JPG"}
+          agreeCount={580}
+          disagreeCount={420}
+          title={'No Manhole Cover | May nahulog na!'}
+          name={'Ronald James Dizon'}
+          date={'December 07, 2021'}
+          description={
+            "There's an open manhole along the street of Mapayapa. Looking forward to cover this Mayor!"
+          }
+          starCount={12}
+          topic={'concern'}
+          imageSrc={
+            'http://4.bp.blogspot.com/-hsqL4cku7NM/VD4fvqd0zeI/AAAAAAAAXOg/Q-uukYGHEII/s1600/DSCN1266.JPG'
+          }
         />
       </div>
     </>
-  );
+  )
 };
 
 export default Dashboard;
